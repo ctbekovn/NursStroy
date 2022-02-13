f92e82525424eb47326208eae3a9b387ccf5d19f
Toktorov <ktoktorov2005@gmail.com>
2022-02-13 21:35:53 +0600

    load static

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from apps.home.models import Setting
 
 # Create your views here.
+def index(request):
+    return render(request, 'header-2.html')
--- nurstroy/settings.py
+++ nurstroy/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
