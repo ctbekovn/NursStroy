d5e535c62f300141b45a1cb8713b83ec91a32560
ctbekovn <sejitbekovnursultan@gmail.com>
2022-02-15 13:46:46 +0600

    news in index and object

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from apps.home.models import Setting
 from apps.objects.models import Objects, ObjectsImages
+from apps.news.models import News
 
 # Create your views here.
 def index(request):
     setting = Setting.objects.get(pk = 1)
     last_objects = Objects.objects.all().order_by('-created')[:4]
+    news = News.objects.all()
     context = {
         'setting' : setting, 
-        'last_objects' : last_objects
+        'last_objects' : last_objects,
+        'news' : news,
     }
     return render(request, 'index.html', context)
